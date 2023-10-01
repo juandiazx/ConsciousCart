5c4fb24a4491c238852d1cf6abedd7223806a39a
Robert <oratorio@udel.edu>
2023-10-01 13:05:38 -0400

    pi charts working

--- app.py
+++ app.py
@@ -26,11 +26,32 @@ def hello_world():
 
             sentence_tfidf = tfidf_vectorizer.transform([text_content])
             probabilities = clf.predict_proba(sentence_tfidf)
-            sent_score = str(probabilities[0][1])
+            sent_score = probabilities[0][1]
             bio_score, materials = calculate_normalized_good_score_and_list_materials(text_content)
             harm_score, chemicals = calculate_normalized_harmfulness_score_and_list_materials(text_content)
-            return jsonify({'sent_score': sent_score, 'bio_score': bio_score, 'materials': list(materials),
-                            'harm_score': harm_score, 'chemicals': list(chemicals)})
+            env = sent_score * 2.5
+            if env >= 1:
+                env = 0.99
+            safety = bio_score - 2*harm_score
+            if safety <= -2:
+                safety = 0.2
+            elif safety <= -1:
+                safety = 0.3
+            elif safety <= 0:
+                safety = 0.45
+            elif safety <= 1:
+                safety = 0.55
+            elif safety <= 2:
+                safety = 0.65
+            elif safety <= 3:
+                safety = 0.75
+            elif safety <= 4:
+                safety = 0.85
+            else:
+                safety = 0.95
+            env = int(env * 100)
+            safety = int(safety * 100)
+            return jsonify({'env_score': env, 'safety_score': safety})
         except Exception as e:
             return jsonify({'error': str(e)}), 500  # Return the error message with 500 status code
 
